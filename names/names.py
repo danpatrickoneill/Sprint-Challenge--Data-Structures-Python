a63fbe10cf3c630fe80b2636a8f107b730f919a5
danpatrickoneill <dan47573@gmail.com>
2019-10-18 11:25:06 -0400

    Added runtime complexity comment

--- names/names.py
+++ names/names.py
@@ -11,6 +11,13 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Original search code: runtime complexity O(n**2)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 names_1_bst = BinarySearchTree(names_1[0])
 for name in names_1:
     names_1_bst.insert(name)
