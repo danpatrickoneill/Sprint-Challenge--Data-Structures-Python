fccff4c74999880f3cf226b4ead290cd34e78e69
danpatrickoneill <dan47573@gmail.com>
2019-11-15 11:26:29 -0500

    Implemented reverse list method: tests passing

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,47 +1,78 @@
 class Node:
-  def __init__(self, value=None, next_node=None):
-    # the value at this linked list node
-    self.value = value
-    # reference to the next node in the list
-    self.next_node = next_node
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        self.next_node = next_node
 
-  def get_value(self):
-    return self.value
+    def get_value(self):
+        return self.value
 
-  def get_next(self):
-    return self.next_node
+    def get_next(self):
+        return self.next_node
 
-  def set_next(self, new_next):
-    # set this node's next_node reference to the passed in node
-    self.next_node = new_next
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
+
 
 class LinkedList:
-  def __init__(self):
-    # reference to the head of the list
-    self.head = None
+    def __init__(self):
+        # reference to the head of the list
+        self.head = None
 
-  def add_to_head(self, value):
-    node = Node(value)
-    if self.head is not None:
-      node.set_next(self.head)
-    
-    self.head = node
+    # def __str__(self):
+    #     pass
 
-  def contains(self, value):
-    if not self.head:
-      return False
-    # get a reference to the node we're currently at; update this as we traverse the list
-    current = self.head
-    # check to see if we're at a valid node 
-    while current:
-      # return True if the current value we're looking at matches our target value
-      if current.get_value() == value:
-        return True
-      # update our current node to the current node's next node
-      current = current.get_next()
-    # if we've gotten here, then the target node isn't in our list
-    return False
+    def add_to_head(self, value):
+        node = Node(value)
+        if self.head is not None:
+            node.set_next(self.head)
 
-  def reverse_list(self):
-    # TO BE COMPLETED
-    pass
+        self.head = node
+
+    def contains(self, value):
+        if not self.head:
+            return False
+        # get a reference to the node we're currently at; update this as we traverse the list
+        current = self.head
+        # check to see if we're at a valid node
+        while current:
+            # return True if the current value we're looking at matches our target value
+            if current.get_value() == value:
+                return True
+            # update our current node to the current node's next node
+            current = current.get_next()
+        # if we've gotten here, then the target node isn't in our list
+        return False
+
+    def reverse_list(self):
+        previous_node = None
+        current_node = self.head
+        if self.head is None or self.head.get_next() is None:
+            return
+        next_node = current_node.get_next()
+
+        while next_node:
+            current_node.set_next(previous_node)
+            previous_node = current_node
+            current_node = next_node
+            next_node = next_node.get_next()
+
+        current_node.set_next(previous_node)
+
+        self.head = current_node
+
+
+if __name__ == "__main__":
+    sll = LinkedList()
+    sll.add_to_head(1)
+    sll.add_to_head(2)
+    sll.add_to_head(3)
+    sll.add_to_head(4)
+    sll.add_to_head(5)
+    print(sll.head.value, 5)
+    sll.reverse_list()
+    print(sll.head.value, 1)
+    print(sll.head.get_next().value, 2)
+    print(sll.head.get_next().get_next().value, 3)
