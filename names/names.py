24660ff9124b341e819038f7140f800994523fad
danpatrickoneill <dan47573@gmail.com>
2019-11-15 13:50:02 -0500

    Added starter code w/ comment; removed dupe timers

--- names/names.py
+++ names/names.py
@@ -11,6 +11,13 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Starter solution: runtime complexity O(n**2)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 # MVP solution
 # names_one = {name: 1 for name in names_1}
 
@@ -19,39 +26,31 @@ f.close()
 #     if name_2 in names_one:
 #         duplicates.append(name_2)
 
-# end_time = time.time()
-# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print(f"runtime: {end_time - start_time} seconds")
-
 # BST solution
+# duplicates = []
+# names_1_tree = BinarySearchTree(names_1[0])
+# for i in range(1, len(names_1)):
+#     names_1_tree.insert(names_1[i])
+# for name in names_2:
+#     if names_1_tree.contains(name):
+#         duplicates.append(name)
+
+# Stretch solution
 duplicates = []
-names_1_tree = BinarySearchTree(names_1[0])
-for i in range(1, len(names_1)):
-    names_1_tree.insert(names_1[i])
-for name in names_2:
-    if names_1_tree.contains(name):
-        duplicates.append(name)
+names_1.sort()
+names_2.sort()
+index_1 = 0
+index_2 = 0
+while index_1 < len(names_1) and index_2 < len(names_2):
+    if names_1[index_1] == names_2[index_2]:
+        duplicates.append(names_1[index_1])
+        index_1 += 1
+        index_2 += 1
+    elif names_1[index_1] <= names_2[index_2]:
+        index_1 += 1
+    else:
+        index_2 += 1
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
-
-# Stretch solution
-# duplicates = []
-# names_1.sort()
-# names_2.sort()
-# index_1 = 0
-# index_2 = 0
-# while index_1 < len(names_1) and index_2 < len(names_2):
-#     if names_1[index_1] == names_2[index_2]:
-#         duplicates.append(names_1[index_1])
-#         index_1 += 1
-#         index_2 += 1
-#     elif names_1[index_1] <= names_2[index_2]:
-#         index_1 += 1
-#     else:
-#         index_2 += 1
-
-# end_time = time.time()
-# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print(f"runtime: {end_time - start_time} seconds")
